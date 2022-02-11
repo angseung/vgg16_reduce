030280635b7833df61f336d4ae3ed115a00d0860
angseung <angseung@keti.re.kr>
2022-02-11 15:51:47 +0900

    Added true and false image plotting part(2)...

--- resnet50_seam_carving.py
+++ resnet50_seam_carving.py
@@ -3,6 +3,7 @@ import torch
 from PIL import Image
 import torchvision
 from torchvision import transforms, models as models
+
 # import seam_carving
 from matplotlib import pyplot as plt
 from carve import resize
@@ -17,7 +18,7 @@ else:
     device = "cpu"
 
 model_list = [
-    torch.hub.load('pytorch/vision:v0.10.0', 'resnet50', pretrained=True),
+    torch.hub.load("pytorch/vision:v0.10.0", "resnet50", pretrained=True),
     # torch.hub.load('pytorch/vision:v0.10.0', 'vgg16', pretrained=True),
     # torch.hub.load('pytorch/vision:v0.10.0', 'shufflenet_v2_x1_0', pretrained=True),
     # torch.hub.load('pytorch/vision:v0.10.0', 'inception_v3', pretrained=True),
@@ -27,15 +28,16 @@ model_list = [
     # models.resnext50_32x4d(pretrained=True),
 ]
 
+
 class SeamCarvingResize:
-    def __init__(self, size, energy_mode='backward'):
+    def __init__(self, size, energy_mode="backward"):
         if isinstance(size, int):
             self.target_size = (size, size)
         elif isinstance(size, tuple):
             if len(size) == 2:
                 self.target_size = size
             else:
-                raise ValueError('Size must be int or tuple with length 2 (h, w)')
+                raise ValueError("Size must be int or tuple with length 2 (h, w)")
 
         self.energy_mode = energy_mode  # 'forward' or 'backward'
 
@@ -45,29 +47,36 @@ class SeamCarvingResize:
 
         # dst = seam_carving.resize(
         dst = resize(
-            image, (self.target_size[0], self.target_size[1]),
+            image,
+            (self.target_size[0], self.target_size[1]),
             energy_mode=self.energy_mode,  # Choose from {backward, forward}
-            order='width-first',  # Choose from {width-first, height-first}
-            keep_mask=None
+            order="width-first",  # Choose from {width-first, height-first}
+            keep_mask=None,
         )
 
         return Image.fromarray(dst)
 
     def __repr__(self):
-        return self.__class__.__name__ + "(target_size=%d, energe_mode=%s)" % (self.target_size[0], self.energy_mode)
+        return self.__class__.__name__ + "(target_size=%d, energe_mode=%s)" % (
+            self.target_size[0],
+            self.energy_mode,
+        )
+
 
 input_size = 128
 resize_size = int(input_size * 256 / 224)
 interpolation = torchvision.transforms.InterpolationMode.BILINEAR
 energy_mode = "backward"
 
-preprocess = transforms.Compose([
-    transforms.Resize(resize_size, interpolation=interpolation),
-    SeamCarvingResize(resize_size, energy_mode=energy_mode),
-    transforms.CenterCrop(input_size),
-    transforms.ToTensor(),
-    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-])
+preprocess = transforms.Compose(
+    [
+        transforms.Resize(resize_size, interpolation=interpolation),
+        SeamCarvingResize(resize_size, energy_mode=energy_mode),
+        transforms.CenterCrop(input_size),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+    ]
+)
 
 dataset = torchvision.datasets.ImageNet(
     root="C:/imagenet/", split="val", transform=preprocess
@@ -82,7 +91,11 @@ test_loader = torch.utils.data.DataLoader(
 with torch.no_grad():
     for model in model_list:
         # with open("%s.txt" % model._get_name(), "w") as f:
-        f = open("%s_%d_%d_%s.txt" % (model._get_name(), resize_size, input_size, energy_mode), "w")
+        f = open(
+            "%s_%d_%d_%s.txt"
+            % (model._get_name(), resize_size, input_size, energy_mode),
+            "w",
+        )
         model.eval()
         model_summary = summary(model, input_size=(1, 3, input_size, input_size))
         f.write(str(model_summary) + "\n\n")
@@ -101,22 +114,51 @@ with torch.no_grad():
 
             total += labels.size(0)
             correct += (predicted == labels).sum().item()
-            correct_5 += torch.eq(predicted_top5, labels.view(-1, 1)).sum().float().item()
+            correct_5 += (
+                torch.eq(predicted_top5, labels.view(-1, 1)).sum().float().item()
+            )
 
-            print("Top-1 Accuracy of the network on the [%d] test images: [%f]" % (total, correct / total))
-            f.write("Top-1 Accuracy of the network on the [%d] test images: [%f]\n" % (total, correct / total))
-            print("Top-5 Accuracy of the network on the [%d] test images: [%f]" % (total, correct_5 / total))
-            f.write("Top-5 Accuracy of the network on the [%d] test images: [%f]\n" % (total, correct_5 / total))
+            print(
+                "Top-1 Accuracy of the network on the [%d] test images: [%f]"
+                % (total, correct / total)
+            )
+            f.write(
+                "Top-1 Accuracy of the network on the [%d] test images: [%f]\n"
+                % (total, correct / total)
+            )
+            print(
+                "Top-5 Accuracy of the network on the [%d] test images: [%f]"
+                % (total, correct_5 / total)
+            )
+            f.write(
+                "Top-5 Accuracy of the network on the [%d] test images: [%f]\n"
+                % (total, correct_5 / total)
+            )
 
             top_1_list = (predicted == labels).detach().cpu().numpy().astype(np.bool_)
-            top_5_list = torch.eq(predicted_top5, labels.view(-1, 1)).sum(axis=1).detach().cpu().numpy().astype(np.bool_)
+            top_5_list = (
+                torch.eq(predicted_top5, labels.view(-1, 1))
+                .sum(axis=1)
+                .detach()
+                .cpu()
+                .numpy()
+                .astype(np.bool_)
+            )
 
             ## True case plot...
             true_image_idx = np.where(top_1_list == True)[0][-1]
             true_image_num = i * test_loader.batch_size + true_image_idx
-            true_image = Image.open(test_loader.dataset.imgs[true_image_num][0].replace("\\", "/")).convert("RGB")
-            true_image_resized = transforms.Resize(resize_size, interpolation=interpolation)(true_image)
-            true_image_sc = np.array(SeamCarvingResize(resize_size, energy_mode=energy_mode)(true_image_resized))
+            true_image = Image.open(
+                test_loader.dataset.imgs[true_image_num][0].replace("\\", "/")
+            ).convert("RGB")
+            true_image_resized = transforms.Resize(
+                resize_size, interpolation=interpolation
+            )(true_image)
+            true_image_sc = np.array(
+                SeamCarvingResize(resize_size, energy_mode=energy_mode)(
+                    true_image_resized
+                )
+            )
             true_image_label = predicted.detach().cpu().numpy()[true_image_idx]
             true_image_label_5 = predicted_top5.detach().cpu().numpy()[true_image_idx]
             true_label = dataset.classes[true_image_label][0]
@@ -134,49 +176,102 @@ with torch.no_grad():
             plt.imshow(true_image_sc, aspect="equal")
 
             plt.subplot(313)
-            df = pd.DataFrame(prob_top5.reshape(-1, 5), columns=[true_label_5[0][0], true_label_5[1][0], true_label_5[2][0], true_label_5[3][0], true_label_5[4][0]])
-            plt.table(cellText=df.values, colLabels=df.columns, loc='center')
+            df = pd.DataFrame(
+                prob_top5.reshape(-1, 5),
+                columns=[
+                    true_label_5[0][0],
+                    true_label_5[1][0],
+                    true_label_5[2][0],
+                    true_label_5[3][0],
+                    true_label_5[4][0],
+                ],
+            )
+            plt.table(cellText=df.values, colLabels=df.columns, loc="center")
             plt.axis("off")
 
-            plt.suptitle("[%s] top-1 True, top-5 True \n%s" % (ground_truth, test_loader.dataset.imgs[true_image_num][0].replace("\\", "/")))
+            plt.suptitle(
+                "[%s] top-1 True, top-5 True \n%s"
+                % (
+                    ground_truth,
+                    test_loader.dataset.imgs[true_image_num][0].replace("\\", "/"),
+                )
+            )
             plt.tight_layout()
             # plt.show()
-            fig1.savefig("outs/True_%s" % test_loader.dataset.imgs[true_image_num][0].replace("\\", "/").split("/")[-1], dpi=200)
+            fig1.savefig(
+                "outs/True_%s"
+                % test_loader.dataset.imgs[true_image_num][0]
+                .replace("\\", "/")
+                .split("/")[-1],
+                dpi=200,
+            )
             plt.close(fig1)
 
             ## Flase case plot...
-            true_image_idx = np.where(top_1_list == False)[0][-1]
-            true_image_num = i * test_loader.batch_size + true_image_idx
-            true_image = Image.open(test_loader.dataset.imgs[true_image_num][0].replace("\\", "/")).convert("RGB")
-            true_image_resized = transforms.Resize(resize_size, interpolation=interpolation)(true_image)
-            true_image_sc = np.array(SeamCarvingResize(resize_size, energy_mode=energy_mode)(true_image_resized))
-            true_image_label = predicted.detach().cpu().numpy()[true_image_idx]
-            true_image_label_5 = predicted_top5.detach().cpu().numpy()[true_image_idx]
-            true_label = dataset.classes[true_image_label][0]
-            true_label_5 = np.array(dataset.classes, dtype=object)[true_image_label_5]
-            true_prob = torch.nn.Softmax(dim=1)(outputs)[true_image_idx]
-            prob_top5, _ = torch.topk(true_prob, 5)
+            false_image_idx = np.where(top_1_list == False)[0][-1]
+            false_image_num = i * test_loader.batch_size + false_image_idx
+            false_image = Image.open(
+                test_loader.dataset.imgs[false_image_num][0].replace("\\", "/")
+            ).convert("RGB")
+            false_image_resized = transforms.Resize(
+                resize_size, interpolation=interpolation
+            )(false_image)
+            false_image_sc = np.array(
+                SeamCarvingResize(resize_size, energy_mode=energy_mode)(
+                    false_image_resized
+                )
+            )
+            false_image_label = predicted.detach().cpu().numpy()[false_image_idx]
+            false_image_label_5 = predicted_top5.detach().cpu().numpy()[false_image_idx]
+            false_label = dataset.classes[false_image_label][0]
+            false_label_5 = np.array(dataset.classes, dtype=object)[false_image_label_5]
+            false_prob = torch.nn.Softmax(dim=1)(outputs)[false_image_idx]
+            prob_top5, _ = torch.topk(false_prob, 5)
             prob_top5 = prob_top5.detach().cpu().numpy().astype(np.float32)
-            ground_truth = dataset.classes[labels[true_image_idx].item()][0]
+            ground_truth = dataset.classes[labels[false_image_idx].item()][0]
 
             fig2 = plt.figure(2)
             plt.subplot(311)
-            plt.imshow(true_image, aspect="equal")
+            plt.imshow(false_image, aspect="equal")
 
             plt.subplot(312)
-            plt.imshow(true_image_sc, aspect="equal")
+            plt.imshow(false_image_sc, aspect="equal")
 
             plt.subplot(313)
-            df = pd.DataFrame(prob_top5.reshape(-1, 5), columns=[true_label_5[0][0], true_label_5[1][0], true_label_5[2][0], true_label_5[3][0], true_label_5[4][0]])
-            plt.table(cellText=df.values, colLabels=df.columns, loc='center')
+            df = pd.DataFrame(
+                prob_top5.reshape(-1, 5),
+                columns=[
+                    false_label_5[0][0],
+                    false_label_5[1][0],
+                    false_label_5[2][0],
+                    false_label_5[3][0],
+                    false_label_5[4][0],
+                ],
+            )
+            plt.table(cellText=df.values, colLabels=df.columns, loc="center")
             plt.axis("off")
 
-            plt.suptitle("[%s] top-1 False, top-5 %s \n%s" % (ground_truth, bool(torch.eq(predicted_top5, labels.view(-1, 1)).sum(axis=1)[true_image_idx].item()),
-                                                      test_loader.dataset.imgs[true_image_num][0].replace("\\", "/")))
+            plt.suptitle(
+                "[%s] top-1 False, top-5 %s \n%s"
+                % (
+                    ground_truth,
+                    bool(
+                        torch.eq(predicted_top5, labels.view(-1, 1))
+                        .sum(axis=1)[false_image_idx]
+                        .item()
+                    ),
+                    test_loader.dataset.imgs[false_image_num][0].replace("\\", "/"),
+                )
+            )
             plt.tight_layout()
             # plt.show()
-            fig2.savefig("outs/False_%s" % test_loader.dataset.imgs[true_image_num][0].replace("\\", "/").split("/")[-1], dpi=200)
+            fig2.savefig(
+                "outs/False_%s"
+                % test_loader.dataset.imgs[false_image_num][0]
+                .replace("\\", "/")
+                .split("/")[-1],
+                dpi=200,
+            )
             plt.close(fig2)
-
 
         f.close()
